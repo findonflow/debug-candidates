4bf7686627e783341454ec909798a80dc8cb58c5
Bjarte Stien Karlsen <bjarte@bjartek.org>
2023-11-06 16:04:25 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,74 @@
+module github.com/findonflow/debug_candidates
+
+go 1.20
+
+require (
+	github.com/dgraph-io/badger/v2 v2.2007.4
+	github.com/findonflow/blockexplorer-database-service v0.14.3
+	github.com/onflow/cadence v0.9.2
+	github.com/onflow/flow-go v0.10.1
+	github.com/samber/lo v1.38.1
+	github.com/vmihailenco/msgpack/v4 v4.3.12
+	gorm.io/datatypes v1.2.0
+)
+
+require (
+	github.com/btcsuite/btcd v0.20.1-beta // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgraph-io/ristretto v0.0.3-0.20200630154024-f66de99634de // indirect
+	github.com/dgryski/go-farm v0.0.0-20190423205320-6a90982ecee2 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/ethereum/go-ethereum v1.9.13 // indirect
+	github.com/fxamacker/cbor/v2 v2.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang/protobuf v1.4.0 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.2 // indirect
+	github.com/jackc/puddle/v2 v2.2.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jrick/bitset v1.0.0 // indirect
+	github.com/klauspost/compress v1.12.3 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/oklog/ulid/v2 v2.1.0 // indirect
+	github.com/onflow/flow-go/crypto v0.9.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/raviqqe/hamt v0.0.0-20190615202029-864fb7caef85 // indirect
+	github.com/rivo/uniseg v0.1.0 // indirect
+	github.com/rs/zerolog v1.29.1 // indirect
+	github.com/segmentio/fasthash v1.0.2 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/vgarvardt/backoff v1.0.0 // indirect
+	github.com/vgarvardt/gue/v5 v5.3.0 // indirect
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	github.com/vmihailenco/tagparser v0.1.1 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	go.opentelemetry.io/otel v1.16.0 // indirect
+	go.opentelemetry.io/otel/metric v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	google.golang.org/protobuf v1.23.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.4.7 // indirect
+	gorm.io/driver/postgres v1.5.0 // indirect
+	gorm.io/gorm v1.24.7-0.20230306060331-85eaf9eeda11 // indirect
+)
+
+replace github.com/dapperlabs/flow-go/crypto => github.com/dapperlabs/flow-go/crypto v0.3.2-0.20200312195452-df4550a863b7
---
+++ main.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+	"sort"
+
+	"github.com/dgraph-io/badger/v2"
+	"github.com/onflow/flow-go/model/flow"
+	"github.com/samber/lo"
+	"github.com/vmihailenco/msgpack/v4"
+)
+
+type Tx struct {
+	Body   flow.TransactionBody
+	Result flow.TransactionResult
+}
+
+func main() {
+	spork := "mainnet1"
+
+	db, err := badger.Open(badger.DefaultOptions(fmt.Sprintf("../../spork/%s", spork)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	headers := GetHeaders(db)
+
+	transactions := GetTransactions(db)
+	transactionResults := GetTransactionResults(db)
+
+	groupedHeaders := lo.GroupBy(lo.Values(headers), func(item flow.Header) uint64 {
+		return item.Height
+	})
+
+	txByBlock := map[string][]Tx{}
+	for txId, tx := range transactions {
+		txr := transactionResults[txId]
+
+		t := Tx{
+			Body:   tx,
+			Result: txr.Result,
+		}
+
+		old, ok := txByBlock[txr.BlockId]
+		if !ok {
+			old = []Tx{t}
+		} else {
+			old = append(old, t)
+		}
+		//	fmt.Print(".")
+		txByBlock[txr.BlockId] = old
+	}
+
+	blocks := map[uint64][]Tx{}
+
+	totalTx := 0
+	for _, headerList := range groupedHeaders {
+
+		var header flow.Header
+		var blockId string
+		var txList []Tx
+		var txLength int
+
+		// we sort the headers to get the one with the largest view first
+		sort.SliceStable(headerList, func(i, j int) bool {
+			return headerList[i].View < headerList[j].View
+		})
+
+		for _, hc := range headerList {
+
+			blockId = hc.ID().String()
+			txList = txByBlock[blockId]
+			if txLength > 0 && len(txList) > 0 {
+				fmt.Printf("Height %d has two or more blockIds with transactions\n", header.Height)
+				fmt.Printf("%s = %d\n", header.ID().String(), txLength)
+				fmt.Printf("%s = %d\n", blockId, len(txList))
+			}
+			txLength = len(txList)
+			header = hc
+		}
+		blocks[header.Height] = txList
+	}
+
+	fmt.Println("total blocks:", len(blocks))
+	fmt.Println("total tx:", totalTx)
+}
+
+type Headers = map[string]flow.Header
+
+func GetHeaders(db *badger.DB) Headers {
+	tx := db.NewTransaction(false)
+
+	it := tx.NewIterator(badger.DefaultIteratorOptions)
+
+	prefix := make([]byte, 1)
+	prefix[0] = 30
+	headers := Headers{}
+	for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+		var header flow.Header
+		item := it.Item()
+
+		k := item.Key()
+		blockID := hex.EncodeToString(k[1:33])
+
+		valueErr := item.Value(func(val []byte) error {
+			umarshalErr := msgpack.Unmarshal(val, &header)
+			if umarshalErr != nil {
+
+				var test map[string]interface{}
+
+				msgpack.Unmarshal(val, &test)
+				fmt.Println("failed unpacking for block", blockID)
+				fmt.Println(test["Height"])
+
+			}
+			headers[blockID] = header
+
+			return nil
+		})
+
+		if valueErr != nil {
+			panic(valueErr)
+		}
+	}
+
+	return headers
+}
+
+func GetTransactionResults(db *badger.DB) map[string]IndexerTransactionResult {
+	tx := db.NewTransaction(false)
+
+	it := tx.NewIterator(badger.DefaultIteratorOptions)
+
+	prefix := make([]byte, 1)
+	prefix[0] = 104
+	transactionResults := map[string]IndexerTransactionResult{}
+	for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+		var transactionResult flow.TransactionResult
+		item := it.Item()
+
+		k := item.Key()
+		blockID := hex.EncodeToString(k[1:33])
+		transactionID := hex.EncodeToString(k[33:65])
+
+		valueErr := item.Value(func(val []byte) error {
+			umarshalErr := msgpack.Unmarshal(val, &transactionResult)
+			if umarshalErr != nil {
+				return umarshalErr
+			}
+			transactionResults[transactionID] = IndexerTransactionResult{
+				Result:  transactionResult,
+				BlockId: blockID,
+			}
+
+			return nil
+		})
+
+		if valueErr != nil {
+			panic(valueErr)
+		}
+	}
+
+	return transactionResults
+}
+
+type IndexerTransactionResult struct {
+	Result  flow.TransactionResult
+	BlockId string
+}
+
+type IndexerTransaction struct {
+	Result flow.TransactionBody
+}
+
+func GetTransactions(db *badger.DB) map[string]flow.TransactionBody {
+	tx := db.NewTransaction(false)
+
+	it := tx.NewIterator(badger.DefaultIteratorOptions)
+
+	prefix := make([]byte, 1)
+	prefix[0] = 34
+
+	transactions := map[string]flow.TransactionBody{}
+
+	for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+		var transactionBody flow.TransactionBody
+		item := it.Item()
+
+		k := item.Key()
+		transactionID := hex.EncodeToString(k[1:33])
+		valueErr := item.Value(func(val []byte) error {
+			umarshalErr := msgpack.Unmarshal(val, &transactionBody)
+			if umarshalErr != nil {
+				return umarshalErr
+			}
+
+			transactions[transactionID] = transactionBody
+			return nil
+		})
+
+		if valueErr != nil {
+			panic(valueErr)
+		}
+	}
+
+	return transactions
+}
